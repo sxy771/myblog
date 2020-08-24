6ad5ad1085fccd6a847aef2802d4da15c96b6d7b
Seunghyeon Yeon <tmdgus904@gmail.com>
2020-08-24 12:14:39 +0000

    upload the form of registration page.

--- mysite/users/urls.py
+++ mysite/users/urls.py
@@ -7,4 +7,5 @@ app_name = 'users'
 
 urlpatterns = [
     path('agreement/', views.AgreementView.as_view(), name='agreement'),
+    path('csregister/', views.CsRegisterView.as_view(), name='csregister'),
 ]
--- mysite/users/views.py
+++ mysite/users/views.py
@@ -4,8 +4,10 @@ from django.shortcuts import render, redirect
 from django.utils.decorators import method_decorator
 from .decorators import logout_message_required
 from .models import User
-from django.views.generic import View
+from django.views.generic import View, CreateView
 from django.contrib import messages
+from .forms import CsRegisterForm
+from django.core.exceptions import PermissionDenied
 
 #@method_decorator(logout_message_required, name='dispatch')
 class AgreementView(View):
@@ -24,3 +26,22 @@ class AgreementView(View):
         else:
             messages.info(request, "약관에 모두 동의해주세요.")
             return render(request, 'users/agreement.html')
+
+class CsRegisterView(CreateView):
+    model = User
+    template_name = 'users/register_cs.html'
+    form_class = CsRegisterForm
+
+    def get(self, request, *args, **kwargs):
+        if not request.session.get('agreement', False):
+            raise PermissionDenied
+        request.session['agreement'] = False
+        return super().get(request, *args, **kwargs)
+
+    def get_success_url(self):
+        messages.success(self.request, "회원가입 성공.")
+        return redirect('users:login')
+
+    def form_valid(self, form):
+        self.object = form.save()
+        return redirect(self.get_success_url())
